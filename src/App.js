5680fd83a962533ccb49c6b0501612a50e4518d4
bhargav <bhargavpatel6040@gmail.com>
2020-02-05 20:13:38 +0530

    Adding state to the app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, I'm Bhargav</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello, I\'m Bhargav'
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.state.string}</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={() => this.setState({ string: 'Hello Dev Community!' })}>Change name</button>
+        </header>
+      </div>
+    ); 
+  }
 }
 
 export default App;
