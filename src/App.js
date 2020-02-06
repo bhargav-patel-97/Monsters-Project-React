86cbb71968fb2cc10bd9932b22a26a05ed88144d
bhargav <bhargavpatel6040@gmail.com>
2020-02-06 15:38:47 +0530

    Adding card & card-list components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { CardList } from './components/card-list/card-list.component';
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      string: 'Hello, I\'m Bhargav'
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => this.setState({ monsters: json }));
+
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>{this.state.string}</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button onClick={() => this.setState({ string: 'Hello Dev Community!' })}>Change name</button>
-        </header>
+      <CardList monsters={this.state.monsters}/>
       </div>
     ); 
   }
