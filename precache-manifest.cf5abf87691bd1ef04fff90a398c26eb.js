a48bfdfbad17ac5c7f37ca4d933d86a959127772
bhargav <bhargavpatel6040@gmail.com>
2020-02-08 13:47:09 +0530

    Updates

---
+++ precache-manifest.cf5abf87691bd1ef04fff90a398c26eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c51e9869af6f30098d52aaca916246",
+    "url": "/Monsters-Project-React/index.html"
+  },
+  {
+    "revision": "97ad188450686414e6d9",
+    "url": "/Monsters-Project-React/static/css/main.1a3afbdf.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/Monsters-Project-React/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters-Project-React/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ad188450686414e6d9",
+    "url": "/Monsters-Project-React/static/js/main.a4387934.chunk.js"
+  },
+  {
+    "revision": "4c4e120db3d74a30fb76",
+    "url": "/Monsters-Project-React/static/js/runtime-main.95c1fcab.js"
+  }
+]);
